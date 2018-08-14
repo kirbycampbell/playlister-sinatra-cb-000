1a60d52b2985639a29e1885a6b5942eff82bfb8f
Kirby Campbell <jkirbycampbell@gmail.com>
2018-08-14 21:31:41 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180814212706) do
+ActiveRecord::Schema.define(version: 20180814213027) do
 
   create_table "artists", force: :cascade do |t|
+    t.string "name"
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
+  end
+
+  create_table "songs", force: :cascade do |t|
     t.string "name"
   end
 
