9c9d9b31984420546ab3ab90e724224d32f9f1d7
Kirby Campbell <jkirbycampbell@gmail.com>
2018-08-14 23:04:37 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -22,7 +22,9 @@ class SongsController < ApplicationController
     @song.genre_ids = params[:genres]
     @song.save
 
-    erb :'songs/show', locals: {message: "Successfully created song."}
+    flash[:message] = "Successfully created song."
+
+    redirect("/songs/#{@song.slug}")
   end
 
   get '/songs/:slug/edit' do
