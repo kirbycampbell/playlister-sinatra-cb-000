f172de12413b2c7517f42f3138449161ae5def64
Kirby Campbell <jkirbycampbell@gmail.com>
2018-08-14 22:51:38 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -16,7 +16,10 @@ class SongsController < ApplicationController
 
   post '/songs' do
     @song = Song.create(name: params["Name"])
-    
+    @song.artist = Artist.find_or_create_by(name: params["Artist Name"])
+    @song.genre_ids = params[:genres]
+    @song.save
+
     erb :'/songs/show'
   end
 
