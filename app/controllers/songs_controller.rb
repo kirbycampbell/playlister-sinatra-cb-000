8cbdbe389c9ff62360b8134cb9f9d1df42cb68a4
Kirby Campbell <jkirbycampbell@gmail.com>
2018-08-14 22:55:56 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -28,4 +28,12 @@ class SongsController < ApplicationController
     erb :'songs/edit'
   end
 
+  patch '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    @song.update(params[:song])
+    @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
+    @song.save
+    erb :'songs/show', locals: {message: "Song successfully updated."}
+  end
+
 end
