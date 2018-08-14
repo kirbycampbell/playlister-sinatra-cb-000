16ff23c66260e38b424a91cb22247ed75c46747f
Kirby Campbell <jkirbycampbell@gmail.com>
2018-08-14 22:41:52 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -6,8 +6,8 @@ class SongsController < ApplicationController
   end
 
   get '/songs/:slug' do
-  @song = Song.find_by_slug(params[:slug])
-  erb :'songs/show'
-end
+    @song = Song.find_by_slug(params[:slug])
+    erb :'songs/show'
+  end
 
 end
